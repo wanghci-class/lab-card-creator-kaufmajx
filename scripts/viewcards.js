6066024fcf3baab6ad388af120bcd58045e09f2b
kaufmajx <kaufmajx@dukes.jmu.edu>
2024-03-29 00:24:08 -0400

    card displays

--- scripts/viewcards.js
+++ scripts/viewcards.js
@@ -13,16 +13,20 @@ for (let i = 0; i < cards.length; i++) {
     let fromText = cardView.querySelector(".message-text");
     let messageText = cardView.querySelector(".from-text");
 
-    titleText.messageText
+    titleText.innerHTML = card.title;
+    subTTlText.innerHTML = card.subtitle;
+    toText.innerHTML = card.to;
+    fromText.innerHTML = card.from;
+    messageText.innerHTML = card.messsage;
     // TODO The rest of text spans
     let deleteBtn = cardView.querySelector(".delete-btn");
     deleteBtn.addEventListener("click", () => {
         console.log(i);
-        deleteBtn();
     });
     cardList.appendChild(cardView);
 }
 
-function deleteBtn(){
-    
+function deleteBtn() {
+    cards = cards.splice(i, 1);
+    localStorage.setItem("cards", JSON.parse(cards));
 }
